75b0961148571a010aba1f83f586d514d3237b64
Sean McIntyre <sean@redivis.com>
2020-06-11 18:27:08 -0700

    fix vector removal, don't zoom/pan on update (#23)
    * Feature filters (#2)
    
    * filters mvp
    
    * style nits
    
    * Refactor redivis api (#4)
    
    * wip
    
    * Update piping for classes
    
    * Fix rasters
    
    * Update vectors.js
    
    * Use service account credentials
    
    * Update default zoom level
    
    * use redivis.com API
    
    * fix filters
    
    Co-authored-by: Sean McIntyre <sean@redivis.com>
    
    * Update package.json
    
    * Refactor filters (#18)
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * add base maps, single raster selection, styling wip
    
    * revert to old styles
    
    Co-authored-by: Ian Mathews <imathews@users.noreply.github.com>
    
    * nit
    
    * fix layer removal bug, don't zoom/pan on update
    
    Co-authored-by: Ian Mathews <imathews@users.noreply.github.com>

--- src/Map/MapRenderer.js
+++ src/Map/MapRenderer.js
@@ -8,64 +8,106 @@ export default class MapRenderer {
 		this.map = L.map(elem, { preferCanvas: true });
 		L.control.scale({ position: 'bottomright' }).addTo(this.map);
 
-		this.layers = new Map();
+		this.baseLayers = new Map();
+		this.rasterLayers = new Map();
+		this.vectorLayers = new Map();
+		this.minZoomByLayer = new Map();
 
-		this.update(props);
+		this.update(props, true);
 		this.map.on('zoomend', () => {
 			this.updateLayers(this.map.getZoom());
 		});
 	}
 
 	updateLayers = (zoom) => {
-		for (const [layerSpec, layer] of this.layers) {
-			if (layerSpec.kind === 'raster') {
+		for (const [layerName, layer] of this.rasterLayers) {
+			if (!this.map.hasLayer(layer)){
 				this.map.addLayer(layer);
-			} else if (zoom < layerSpec.minZoom) {
+			}
+			if (this.minZoomByLayer.get(layerName) && zoom < this.minZoomByLayer.get(layerName)){
 				this.map.removeLayer(layer);
-			} else {
+			}
+		}
+		for (const [layerName, layer] of this.vectorLayers) {
+			if (!this.map.hasLayer(layer)){
 				this.map.addLayer(layer);
+			}
+			if (this.minZoomByLayer.get(layerName) && zoom < this.minZoomByLayer.get(layerName)){
+				this.map.removeLayer(layer);
 			}
 		}
 	};
 
-	update({ baseMapLayer, rasterLayers, vectorLayers, centroid = [1, 32], zoom = 7 }) {
-		this.map.eachLayer((layer) => {
-			this.map.removeLayer(layer);
-		});
-
-		this.map.setView(centroid, zoom);
-
-		const baseLayer = L.tileLayer('https://api.mapbox.com/v4/{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}', {
-			id: baseMapLayer.mapboxId,
-			opacity: 1,
-			tileSize: 512,
-			zoomOffset: -1,
-			accessToken: 'pk.eyJ1IjoiaW1hdGhld3MiLCJhIjoiY2thdnl2cGVsMGtldTJ6cGl3c2tvM2NweSJ9.TXtG4gARAf4bUbnPVxk6uA',
-		});
-
-		this.map.addLayer(baseLayer);
-
-		this.layers = new Map();
-
-		for (const rasterLayer of rasterLayers) {
-			const layer = L.tileLayer('https://api.mapbox.com/v4/{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}', {
-				id: rasterLayer.mapboxId,
-				opacity: 0.8,
-				tileSize: 512,
-				zoomOffset: -1,
-				accessToken:
-					'pk.eyJ1IjoiaW1hdGhld3MiLCJhIjoiY2thdnl2cGVsMGtldTJ6cGl3c2tvM2NweSJ9.TXtG4gARAf4bUbnPVxk6uA',
-				minNativeZoom: rasterLayer.minNativeZoom + 1,
-				maxNativeZoom: rasterLayer.maxNativeZoom + 1,
-				bounds: rasterLayer.bounds,
-			});
-			this.layers.set(rasterLayer, layer);
+	update({ baseMapLayer, rasterLayers, vectorLayers, centroid = [1, 32], zoom = 7 }, initialRender = false) {
+		if (initialRender){
+			this.map.setView(centroid, zoom);
 		}
 
-		for (const vectorLayer of vectorLayers) {
-			const layer = new L.FeatureGroup();
-			this.layers.set(vectorLayer, layer);
+		let baseLayer = this.baseLayers.get(baseMapLayer.name);
+		if (!baseLayer){
+			for (const [layerName, layer] of this.baseLayers){
+				this.map.removeLayer(layer);
+				this.baseLayers.delete(layerName);
+			}
+			baseLayer = L.tileLayer('https://api.mapbox.com/v4/{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}', {
+				id: baseMapLayer.mapboxId,
+				opacity: 1,
+				tileSize: 512,
+				zoomOffset: -1,
+				accessToken: 'pk.eyJ1IjoiaW1hdGhld3MiLCJhIjoiY2thdnl2cGVsMGtldTJ6cGl3c2tvM2NweSJ9.TXtG4gARAf4bUbnPVxk6uA',
+			});
 
+			this.map.addLayer(baseLayer);
+			this.baseLayers.set(baseMapLayer.name, baseLayer);
+		}
+
+		const rasterLayerNamesSet = new Set(rasterLayers.map(({ name }) => name));
+		for (const [layerName, layer] of this.rasterLayers){
+			if (!rasterLayerNamesSet.has(layerName)){
+				this.map.removeLayer(layer);
+				this.rasterLayers.delete(layerName);
+				this.minZoomByLayer.delete(layerName);
+			}
+		}
+		for (const rasterLayer of rasterLayers) {
+			let layer = this.rasterLayers.get(rasterLayer.name);
+			if (!layer){
+				layer = L.tileLayer('https://api.mapbox.com/v4/{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}', {
+					id: rasterLayer.mapboxId,
+					opacity: 0.8,
+					tileSize: 512,
+					zoomOffset: -1,
+					accessToken:
+						'pk.eyJ1IjoiaW1hdGhld3MiLCJhIjoiY2thdnl2cGVsMGtldTJ6cGl3c2tvM2NweSJ9.TXtG4gARAf4bUbnPVxk6uA',
+					minNativeZoom: rasterLayer.minNativeZoom + 1,
+					maxNativeZoom: rasterLayer.maxNativeZoom + 1,
+					bounds: rasterLayer.bounds,
+				});
+				this.rasterLayers.set(rasterLayer.name, layer);
+				if (rasterLayer.minZoom){
+					this.minZoomByLayer.set(rasterLayer.name, rasterLayer.minZoom);
+				}
+			}
+		}
+
+		const vectorLayerNamesSet = new Set(vectorLayers.map(({ name }) => name));
+		for (const [layerName, layer] of this.vectorLayers){
+			if (!vectorLayerNamesSet.has(layerName)){
+				this.map.removeLayer(layer);
+				this.vectorLayers.delete(layerName);
+				this.minZoomByLayer.delete(layerName);
+			}
+		}
+		for (const vectorLayer of vectorLayers) {
+			let layer = this.vectorLayers.get(vectorLayer.name);
+			if (!layer) {
+				layer = new L.FeatureGroup();
+				this.vectorLayers.set(vectorLayer.name, layer);
+				if (vectorLayer.minZoom){
+					this.minZoomByLayer.set(vectorLayer.name, vectorLayer.minZoom)
+				}
+			}
+			layer.clearLayers();
 			for (const feature of vectorLayer.features) {
 				const marker = L[vectorLayer.leafletType](feature.geometry, vectorLayer.leafletOptions).addTo(layer);
 
