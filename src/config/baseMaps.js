fdcc2746e1f7e7ce51a23aba85c926a828355a98
Sean McIntyre <sean@redivis.com>
2020-06-22 11:54:28 -0700

    update baseMap config

--- src/config/baseMaps.js
+++ src/config/baseMaps.js
@@ -14,26 +14,39 @@ const baseMaps = [
 	// 	name: 'Streets',
 	// 	mapboxId: 'mapbox.mapbox-streets-v8',
 	// }),
+	// {
+	// 	name: 'Streets',
+	// 	mapboxStyle: 'mapbox://styles/mapbox/streets-v11',
+	// 	isDefault: true,
+	// },
 	{
 		name: 'Streets',
-		mapboxStyle: 'mapbox://styles/mapbox/streets-v11',
+		mapboxStyle: 'mapbox://styles/imathews/ckbpms3l74h4e1ipbxbhqivs4',
 		isDefault: true,
+	},
+	{
+		name: 'BasemapSatellite',
+		mapboxStyle: 'mapbox://styles/imathews/ckbjw25s806j01ipj5fjyi9f4',
+	},
+	{
+		name: 'BasemapSimple_WithTopo',
+		mapboxStyle: 'mapbox://styles/imathews/ckbk71nle03i41il6uorr1jpz',
+	},
+	{
+		name: 'SatelliteSimple_MonochromeDark',
+		mapboxStyle: 'mapbox://styles/imathews/ckbl1ek0e00g51ipskyv1xtpb',
+	},
+	{
+		name: 'BasemapSimple',
+		mapboxStyle: 'mapbox://styles/imathews/ckbjwv03707b21ipjjukqohfw',
+	},
+	{
+		name: 'RoadNetwork_LowOpacityRaster',
+		mapboxStyle: 'mapbox://styles/imathews/ckbk765ng03l11im05dbcfscv',
 	},
 	{
 		name: 'Terrain_RGB_Edit1',
 		mapboxStyle: 'mapbox://styles/imathews/ckbjwd4a804cy1imp0tf8kz90',
-	},
-	{
-		name: 'Satellite_Basemap1',
-		mapboxStyle: 'mapbox://styles/imathews/ckbjw25s806j01ipj5fjyi9f4',
-	},
-	{
-		name: 'Streets_Networks_Basemap',
-		mapboxStyle: 'mapbox://styles/imathews/ckbjwv03707b21ipjjukqohfw',
-	},
-	{
-		name: 'Decimal',
-		mapboxStyle: 'mapbox://styles/imathews/ckbjw724006nh1ipj86huoe0s',
 	},
 ];
 
