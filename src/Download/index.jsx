740717e29b97896ed6c54b303041244d4c139f6a
Ian Mathews <imathews@users.noreply.github.com>
2020-06-25 17:21:58 -0700

    Download page

--- src/Download/index.jsx
+++ src/Download/index.jsx
@@ -1,4 +1,6 @@
 import * as styles from './styles.css';
+import downloads from '../config/downloads';
+console.log(downloads);
 
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
@@ -7,7 +9,27 @@ class About extends Component {
 	render() {
 		return (
 			<div className={styles.wrapper}>
-				<span className={styles.header}>{'Download'}</span>
+				{downloads.map((download, i) => {
+					return (
+						<div key={i} className={styles.downloadItem}>
+							<span className={styles.downloadLabel}>{download.label}</span>
+							<br />
+							{download.links.map((link, i) => {
+								return (
+									<a
+										key={i}
+										className={styles.downloadLink}
+										target={'_blank'}
+										rel={'noopener noreferrer'}
+										href={link.href}
+									>
+										{link.name}
+									</a>
+								);
+							})}
+						</div>
+					);
+				})}
 			</div>
 		);
 	}
